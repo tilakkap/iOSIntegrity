0d2a1c368797e749051bb15b1a41757b3a80a52a
thomas <webhoppe@web.de>
2022-09-27 11:49:34 +0700

    getModel

--- Sources/iOSIntegrity/iOSIntegrity.swift
+++ Sources/iOSIntegrity/iOSIntegrity.swift
@@ -153,23 +153,62 @@ public class iOSIntegrity {
 
     public static func checkPlist(template: URL, OSVersion: String, models: Array<String>) -> Bool {
         let templatePlist = iOSIntegrity.getInfoPlist(url: template);
-
-        for (key, value) in templatePlist! {
-
-        }
-
         return true
     }
 
-    public static func getModel(){
+    public static func getModel() -> String {
         var systemInfo = utsname()
         uname(&systemInfo)
         let machineMirror = Mirror(reflecting: systemInfo.machine)
         let identifier = machineMirror.children.reduce("") { identifier, element in
-            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            guard let value = element.value as? Int8 , value != 0 else { return identifier }
             return identifier + String(UnicodeScalar(UInt8(value)))
         }
+        return identifier
     }
+
+    @objc static func isFridaRunning() -> Bool {
+        func swapBytesIfNeeded(port: in_port_t) -> in_port_t {
+            let littleEndian = Int(OSHostByteOrder()) == OSLittleEndian
+            return littleEndian ? _OSSwapInt16(port) : port
+        }
+
+        var serverAddress = sockaddr_in()
+        serverAddress.sin_family = sa_family_t(AF_INET)
+        serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1")
+        serverAddress.sin_port = swapBytesIfNeeded(port: in_port_t(27042))
+        let sock = socket(AF_INET, SOCK_STREAM, 0)
+
+        let result = withUnsafePointer(to: &serverAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                connect(sock, $0, socklen_t(MemoryLayout<sockaddr_in>.stride))
+            }
+        }
+        if result != -1 {
+            return true
+        }
+        return false
+    }
+
+    @objc static func checkDYLD() -> Bool {
+        let suspiciousLibraries = [
+            "FridaGadget",
+            "frida",
+            "cynject",
+            "libcycript"
+        ]
+        for libraryIndex in 0..<_dyld_image_count() {
+
+            guard let loadedLibrary = String(validatingUTF8: _dyld_get_image_name(libraryIndex)) else { continue }
+            for suspiciousLibrary in suspiciousLibraries {
+                if loadedLibrary.lowercased().contains(suspiciousLibrary.lowercased()) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
 }
 
 
